46e8444932dedbcc53394431ef93d402a8884d6e
Iury Braun <32597667+iurybraun@users.noreply.github.com>
2023-11-07 12:03:07 -0300

    Update client_gateway.go

--- client_gateway.go
+++ client_gateway.go
@@ -25,6 +25,23 @@ func NewClientGateway(client Client) *Gateway {
     }
 }
 
+type PaymentReq struct {
+	Id int `json:"id"`
+	Client_id string `json:"client_id"`
+	Collector_id int `json:"collector_id"`
+	Currency_id string `json:"currency_id"`
+	Date_approved string `json:"date_approved"`
+	External_reference string `json:"external_reference"`
+	Installments int `json:"installments"`
+	Order struct {
+		Id string `json:"id"`
+		Type string `json:"type"`
+	} `json:"order"`
+	Transaction_amount int `json:"transaction_amount"`
+	Captured bool `json:"captured"`
+	Status string `json:"status"`
+}
+
 func (g *Gateway) GetAccessToken(credentials Credentials) (string, error) {
     path := &url.Values{}
     path.Add("client_id", credentials.ClientID)
@@ -62,33 +79,33 @@ func (g *Gateway) GetAccessToken(credentials Credentials) (string, error) {
     return r.AccessToken, nil
 }
 
-func (g *Gateway) CreatePreference(accessToken string, preference NewPreference) (string, error) {
+func (g *Gateway) CreatePreference(accessToken string, preference NewPreference) (string, string, error) {
     queryValues := &url.Values{}
     queryValues.Add("access_token", accessToken)
     queryParams := queryValues.Encode()
 
     b, err := json.Marshal(preference)
     if err != nil {
-        return "", err
+        return "", "", err
     }
 
     req, err := http.NewRequest("POST", fmt.Sprintf("%s%s%s", _baseURL, "/checkout/preferences?", queryParams), bytes.NewReader(b))
     if err != nil {
-        return "", err
+        return "", "", err
     }
 
     resp, err := g.Client.Do(req)
     if err != nil {
-        return "", err
+        return "", "", err
     }
 
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return "", err
+        return "", "", err
     }
 
     if resp.StatusCode >= http.StatusBadRequest {
-        return "", NewError(string(body), resp.StatusCode)
+        return "", "", NewError(string(body), resp.StatusCode)
     }
 
     var r struct {
@@ -98,10 +115,133 @@ func (g *Gateway) CreatePreference(accessToken string, preference NewPreference)
     }
 
     if err := json.Unmarshal(body, &r); err != nil {
-        return "", err
+        return "", "", err
     }
 
     return r.Id, r.CheckoutURL, nil
+}
+
+func (g *Gateway) GetCheckoutPreferences(accessToken string, id string) (int, error) {
+    ///queryValues := &url.Values{}
+    ///queryValues.Add("limit", "1")
+    ///queryValues.Add("offset", "0")
+    ///queryValues.Add("access_token", accessToken)
+    ///queryValues.Add("id", id)
+
+    ///queryParams := queryValues.Encode()
+    
+    req, err := http.NewRequest("GET", _baseURL + "/checkout/preferences/"+id, nil)
+    if err != nil {
+        return 0, err
+    }
+    
+    req.Header.Add("Authorization", "Bearer " + accessToken)
+
+    resp, err := g.Client.Do(req)
+    if err != nil {
+        return 0, err
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return 0, err
+    }
+
+    if resp.StatusCode >= http.StatusBadRequest {
+        return 0, NewError(string(body), resp.StatusCode)
+    }
+    
+    var r struct {
+		Id string `json:"id"`
+		Client_id string `json:"client_id"`
+		Collector_id int `json:"collector_id"`
+		External_reference string `json:"external_reference"`
+		Total_amount int `json:"total_amount"`
+	}
+
+    if err := json.Unmarshal(body, &r); err != nil {
+        return 0, err
+    }
+    
+    return r.Total_amount, nil
+}
+
+func (g *Gateway) GetPayments(accessToken string, id string) (payment PaymentReq, err error) {
+    ///queryValues := &url.Values{}
+    ///queryValues.Add("limit", "1")
+    ///queryValues.Add("offset", "0")
+    ///queryValues.Add("access_token", accessToken)
+    ///queryValues.Add("id", id)
+
+    ///queryParams := queryValues.Encode()
+    
+    req, err := http.NewRequest("GET", _baseURL + "/v1/payments/"+id, nil)
+    if err != nil {
+        return
+    }
+    
+    req.Header.Add("Authorization", "Bearer " + accessToken)
+
+    resp, err := g.Client.Do(req)
+    if err != nil {
+        return
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return
+	}
+
+    if resp.StatusCode >= http.StatusBadRequest {
+        err = NewError(string(body), resp.StatusCode)
+    }
+    
+    r := PaymentReq{}
+    if err := json.Unmarshal(body, &r); err != nil {
+        return payment, err
+    }
+    
+    return
+}
+
+func (g *Gateway) GetPaymentsSearch(accessToken string, external_reference string) (payment PaymentReq, err error) {
+    ///queryValues := &url.Values{}
+    ///queryValues.Add("limit", "1")
+    ///queryValues.Add("offset", "0")
+    ///queryValues.Add("access_token", accessToken)
+    ///queryValues.Add("id", id)
+
+    ///queryParams := queryValues.Encode()
+    
+    //https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc&external_reference=ID_a9XY6Qd+aKTswbX2sdZQ/B0Mzs8pSWnzynl/CR1Ek5Y
+    
+    req, err := http.NewRequest("GET", _baseURL + "/v1/payments/search?sort=date_created&criteria=desc&external_reference="+external_reference, nil)
+    if err != nil {
+        return
+    }
+    
+    req.Header.Add("Authorization", "Bearer " + accessToken)
+
+    resp, err := g.Client.Do(req)
+    if err != nil {
+        return
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return
+	}
+
+    if resp.StatusCode >= http.StatusBadRequest {
+        err = NewError(string(body), resp.StatusCode)
+    }
+    
+    r := PaymentReq{}
+    if err := json.Unmarshal(body, &r); err != nil {
+        return payment, err
+    }
+fmt.Println(r)
+    return
 }
 
 func (g *Gateway) GetTotalPayments(accessToken string, status string) (int, error) {
