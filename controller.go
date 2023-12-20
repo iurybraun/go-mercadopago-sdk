75a1c8b0e903ad5353d120838d9b8140da48da34
Iury Braun <32597667+iurybraun@users.noreply.github.com>
2023-12-20 06:29:45 -0300

    Update controller.go

--- controller.go
+++ controller.go
@@ -2,10 +2,11 @@ package mercadopago
 
 type ClientGateway interface {
     GetAccessToken(credentials Credentials) (string, error)
-    CreatePreference(accessToken string, preference NewPreference) (string, int, string, error)
+    CreatePreference(accessToken string, preference NewPreference) (string, string, error)
     GetCheckoutPreferences(accessToken string, id string) (int, error)
     GetPayments(accessToken string, id string) (PaymentReq, error)
     GetPaymentsSearch(accessToken string, external_reference string) (PaymentReqSearch, error)
+    //GetMerchantOrders(accessToken string, order_id string) (MerchantOrders, error)
     GetTotalPayments(accessToken string, status string) (int, error)
 }
 
@@ -26,7 +27,7 @@ func (s *Controller) GetAccessToken(clientID string, clientSecret string) (strin
     })
 }
 
-func (s *Controller) CreatePreference(accessToken string, preference NewPreference) (string, int, string, error) {
+func (s *Controller) CreatePreference(accessToken string, preference NewPreference) (string, string, error) {
     return s.Client.CreatePreference(accessToken, preference)
 }
 
@@ -41,6 +42,10 @@ func (s *Controller) GetPayments(accessToken string, id string) (PaymentReq, err
 func (s *Controller) GetPaymentsSearch(accessToken string, external_reference string) (PaymentReqSearch, error) {
     return s.Client.GetPaymentsSearch(accessToken, external_reference)
 }
+
+/*func (s *Controller) GetMerchantOrders(accessToken string, order_id string) (MerchantOrders, error) {
+    return s.Client.GetMerchantOrders(accessToken, order_id)
+}*/
 
 func (s *Controller) GetTotalPayments(accessToken string, status string) (int, error) {
     return s.Client.GetTotalPayments(accessToken, status)
